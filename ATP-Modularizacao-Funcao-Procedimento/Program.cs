e20e69b4ae65e1eb961be575ec889ba318807e7a
Sara da Cruz Freitas <159440011+sahfreitas@users.noreply.github.com>
2026-05-11 19:58:31 -0300

    Add files via upload

---
+++ ATP-Modularizacao-Funcao-Procedimento/Program.cs
@@ -0,0 +1,12 @@
+﻿class Progam{
+    public static void Main(){
+        string entrada = "";
+        
+        while(entrada != "fim"){
+            Categoria.Rodar();
+            Media.Rodar();
+            
+            entrada = Console.ReadLine();
+        }
+    }
+ }
